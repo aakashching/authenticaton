82e402ccb69c555f7d4a692959f37d59f4064a2a
aakashching <aakashching@gmail.com>
2021-05-26 17:31:30 +0530

    bcrypt hash

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const app = express();
 
 
@@ -36,17 +38,18 @@ app.get('/login', (req,res)=> {
 })
 app.post('/login', (req,res)=> {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({email: username}, (err, foundUser)=> {
         if(err) {
             console.log(err);
         } else {
             if(foundUser){
-                if(foundUser.password === password) {
-                    res.render('secrets');
-                } else {
-                    res.send('incorrect password')
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result == true
+                    if(result == true) {
+                        res.render('secrets')
+                    }
+                });
             } else {
                 res.send('user not found')
             }
@@ -58,16 +61,22 @@ app.get('/register', (req,res)=> {
     res.render('register')
 })
 app.post ('/register', (req,res)=> {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save((err)=> {
+            if(!err) {
+                console.log('succussfully created new user');
+                res.render('secrets')
+            } else {console.log(err)}
+        });
     });
-    newUser.save((err)=> {
-        if(!err) {
-            console.log('succussfully created new user');
-            res.render('secrets')
-        } else {console.log(err);}
-    });
+    
 })
 
 app.get('/submit', (req,res)=> {
